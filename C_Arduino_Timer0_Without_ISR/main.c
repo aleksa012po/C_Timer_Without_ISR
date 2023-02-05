2593a5c56180ae7bc3ee8412242d4cf98f82ad21
Aleksandar <102791994+aleksa012po@users.noreply.github.com>
2023-02-05 22:35:39 +0100

    Add files via upload

---
+++ C_Arduino_Timer0_Without_ISR/main.c
@@ -0,0 +1,31 @@
+/*
+ * C_Arduino_Interrupt_Without_ISR.c
+ *
+ * Created: 24.11.2022. 12:29:09
+ * Author : Aleksandar Bogdanovic
+ */ 
+
+#define F_CPU 16000000							// 16MHz
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "timerSetup.h"
+#include "init.h"
+
+int extraTime = 0;
+
+int main(void)
+{
+    init();
+	timerSetup();
+    while (1) 
+    {
+		extraTime++;
+		if(extraTime > 255) {
+			PORTB ^= (1 << PORTB0);
+			_delay_ms(500);
+		}	
+    }
+}
+
---
+++ C_Arduino_Timer0_Without_ISR/src/timerSetup.h
@@ -0,0 +1,24 @@
+/*
+ * timerSetup.h
+ *
+ * Created: 24.11.2022. 10:11:49
+ *  Author: Aleksandar Bogdanovic
+ */ 
+
+
+#ifndef TIMERSETUP_H_
+#define TIMERSETUP_H_
+
+
+void timerSetup() {
+	TCCR0A = (1 << WGM01);						// TCCR0A = 0b00000010
+	OCR0A = 156;
+	TIMSK0 = (1 << OCIE0A);						// TIMSK0 = 0b00000010, OCIE0A: Timer/Counter0 Output Compare Match A Interrupt Enable
+	
+	sei();										// Sets the I bit, external interrupt enable
+	
+	TCCR0B = (1 << CS02) | (1 << CS00);			// TCCR0B = 0b00000101 - prescaler clk/1024
+}
+
+
+#endif /* TIMERSETUP_H_ */
